7cbc094ff961dbb868cabed8d53985d828235c36
Brian Smith <brian.smith@riseup.net>
2018-12-07 13:03:23 -0500

    chore: remove init file
    This file shouldn't be here based on modern practices.

--- __init__.py
+++
@@ -1,6 +0,0 @@
-# PyRIC v 1.6 root Distribution directory
-# Do not import from this directory i.e from PyRIC import foobar
-# To install
-#   use pip ('sudo pip install PyRIC')
-# or download latest tarbal and untar.
-# excute either sudo python setup.py or rund from the PyRIC/pyric directory
