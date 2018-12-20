17e98d7ec284ae70f31353a8fc65e00252dd591f
Brian Smith <brian.smith@riseup.net>
2018-12-20 13:28:38 -0500

    test: add test cases for interfaces
    Add test cases for the interfaces function.

---
+++ tests/test_wireless.py
@@ -0,0 +1,33 @@
+from pyric.pyw import interfaces
+from unittest.mock import patch, mock_open
+
+
+@patch(
+    "builtins.open",
+    mock_open(
+        read_data="""Inter-|   Receive                                                |  Transmit
+ face |bytes    packets errs drop fifo frame compressed multicast|bytes    packets errs drop fifo colls carrier compressed
+enp12s0: 1263265923  910548    0   12    0     0          0       190 28743630  303148    0    0    0     0       0          0
+    lo:       0       0    0    0    0     0          0         0        0       0    0    0    0     0       0          0
+"""
+    ),
+    spec=True,
+)
+def test_interfaces_two_interfaces():
+    """Test the interfaces function when two interfaces are available."""
+    assert interfaces() == ["enp12s0", "lo"]
+
+
+@patch(
+    "builtins.open",
+    mock_open(
+        read_data="""Inter-|   Receive                                                |  Transmit
+ face |bytes    packets errs drop fifo frame compressed multicast|bytes    packets errs drop fifo colls carrier compressed
+"""
+    ),
+    spec=True,
+)
+def test_interfaces_no_interface():
+    """Test interfaces function when no interfaces are available."""
+    assert interfaces() == []
+
